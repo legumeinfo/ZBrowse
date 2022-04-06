c7b4eddcdaddda64ff19774696abe9a3ebe06eb4
Sven Gato Redsun <svengato@svengato.com>
2022-04-06 11:45:26 -0600

    Update legumeinfo.org links for new (Jekyll) site rollout

--- zChart.R
+++ zChart.R
@@ -3,12 +3,12 @@ source("common.R")
 provideMultipleURLs <- function(includeGenomicLinkage) {
   paste(
     # From the JSON at this.url, extract the URLs related to this gene.
-    # Note that this.url = 'https://legumeinfo.org/gene_links/' + geneString + '/json'
-    #  (the first part of which has 34 characters)
+    # Note that this.url = 'https://legacy.legumeinfo.org/gene_links/' + geneString + '/json'
+    #  (the first part of which has 41 characters)
     #  and geneString = <5-character species abbreviation>.geneName
     # And for now, add the gene family phylogram URL by hand.
     "$.getJSON(this.url, function(data) {",
-      "var geneString = this.url.substring(34, this.url.indexOf('/json'));",
+      "var geneString = this.url.substring(41, this.url.indexOf('/json'));",
       "var geneName = geneString.substring(6);",
       "var content = '';",
       "if (data.length == 0) {",
@@ -17,7 +17,7 @@ provideMultipleURLs <- function(includeGenomicLinkage) {
         "$.each(data, function(i, obj) {",
           "content = content + '<p><a href=' + obj.href + ' target=_blank>' + obj.text + '</a></p>';",
           "if (i == 0) {",
-            "var urlPhylogram = 'http://legumeinfo.org/chado_gene_phylotree_v2?gene_name=' + geneString;",
+            "var urlPhylogram = 'http://legacy.legumeinfo.org/chado_gene_phylotree_v2?gene_name=' + geneString;",
             "var textPhylogram = 'View LIS gene family phylogram page for : ' + geneName;",
             "content = content + '<p><a href=' + urlPhylogram + ' target=_blank>' + textPhylogram + '</a></p>';",
           "}",
@@ -567,8 +567,8 @@ create_zChart <- function(j, input, values) {
         # Broadcast the gene family to the Genome Context Viewer
         "Shiny.onInputChange('gcvGeneFamily', this.name);",
       "} else {",
-        # Go to the gene family's web page
-        "window.open('https://legumeinfo.org/chado_phylotree/' + this.name);",
+        # Go to the gene family's web page (extract from legfed_v1_0.<family>)
+        "window.open('https://funnotate.legumeinfo.org/?family=' + this.name.substring(12));",
       "}",
       "return false;", # and disable toggling the legend item
     "} !#"
